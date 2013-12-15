13aeab9a42986a0aa7fba3278bf99d8b8fb0e184
Jörg Seebohn <joerg.seebohn@web.de>
2013-12-15 08:29:56 +0100

    Begin java_question catalog(ue)

---
+++ java_question/Answers.java
@@ -0,0 +1,28 @@
+package java_question;
+
+public class Answers {
+
+	/* ----------------
+	 * overloading_null
+	 * ---------------- 
+	 * Answer: 1. "print(SubSubclass1)"
+	 */
+	
+	/* -----------------
+	 * overloading_null2
+	 * ----------------- 
+	 * Answer: 4. Compilation error
+	 * 
+	 * > javac overloading_null2.java
+	 * > overloading_null2.java:37: error: reference to print is ambiguous, 
+	 * > both method print(Subclass2_1) in overloading_null2 and 
+	 * > method print(Subclass2_2) in overloading_null2 match
+	 * > print(null);
+	 */
+
+	/* -----------------
+	 * overloading_null3
+	 * ----------------- 
+	 * Answer: 2. "print(Subclass3_2)"
+	 */	
+}

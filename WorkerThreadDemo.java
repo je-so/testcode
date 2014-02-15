7714a60d89d8bb47fef3228449e41a97957d9f72
Jörg Seebohn <joerg.seebohn@web.de>
2014-02-15 20:15:36 +0100

    Update WorkerThreadDemo.java

--- WorkerThreadDemo.java
+++ WorkerThreadDemo.java
@@ -1,74 +1,66 @@
 package thread;
 
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class WorkerThreadDemo {
 
 	static class TaskQueue {
+		static class Monitor {
+			Lock lock = new ReentrantLock();
+			Condition notFull = lock.newCondition();
+			Condition notEmpty = lock.newCondition();
+		}
+
+		/*
+		 * Explizites Monitor-Objekt.
+		 */
+                private Monitor monitor = new Monitor();
+
 		/*
 		 * Ein Auftrag wird als simpler int repräsentiert.
 		 * Die Queue verwaltet maximal 10 Aufträge.
 		 */
-		int[] tasks = new int[10];
-		int   first_task_index;
-		int   nr_of_task;
+		private Deque<Integer> tasks = new ArrayDeque<>();
+		private static final int MAXSIZE = 10;
 		
-		/*
-		 * Aufgabe: Schreiben Sie removeTask so um, dass die Methode im Falle einer leeren Queue wartet,
-		 * bis ein Task verfügbar ist.
-		 */
 		public int removeTask() throws InterruptedException {
-			int task;
-			while ((task = sync_remove()) == -1) {
-				System.out.println(Thread.currentThread().getName() + ": Warte auf Task");
-				synchronized (tasks) {
-					tasks.wait();
+			monitor.lock.lockInterruptibly();
+			try {
+				while (tasks.size() == 0) {
+					System.out.println(Thread.currentThread().getName() + ": Warte, da Queue leer");
+					monitor.notEmpty.await();
+					System.out.println(Thread.currentThread().getName() + ": Warten zu Ende");
 				}
-				System.out.println(Thread.currentThread().getName() + ": Warten zu Ende");
-			}
-			return task;
-		}
-
-		/*
-		 * Aufgabe: Schreiben Sie addTask so um, dass die Methode im Falle einer vollen Queue wartet,
-		 * bis wieder Platz verfügbar ist.
-		 */
-		public synchronized void addTask(int task) throws InterruptedException {
-			if (task < 0) {
-				throw new IllegalArgumentException("task < 0");
-			}
-			
-			while (nr_of_task == tasks.length) {
-				System.out.println(Thread.currentThread().getName() + ": Warte bis Queue nicht mehr voll");
-				this.wait();
-				System.out.println(Thread.currentThread().getName() + ": Warten zu Ende");
-			}		
-
-			final int index = (first_task_index + nr_of_task) % tasks.length;
-			++ nr_of_task;
-			tasks[index] = task;
-			synchronized (tasks) {
-				tasks.notify(); // wake up possible waiting threads in removeTask
+				monitor.notFull.signal();
+				return tasks.remove();
+			} finally {
+				monitor.lock.unlock();
 			}
 		}
 
-		/*
-		 * Liefert einen Task zurück (>= 0) oder -1, falls
-		 * die Queue leer ist.
-		 */
-		private synchronized int sync_remove() {
-			if (nr_of_task == 0) {
-				return -1;
+		public void addTask(int task) throws InterruptedException {
+			monitor.lock.lockInterruptibly();
+			try {
+				while (tasks.size() == MAXSIZE) {
+					System.out.println(Thread.currentThread().getName() + ": Warte, da Queue voll");
+					monitor.notFull.await();
+					System.out.println(Thread.currentThread().getName() + ": Warten zu Ende");
+				}
+				monitor.notEmpty.signal();
+				tasks.add(task);
+			} finally {
+				monitor.lock.unlock();
 			}
-			-- nr_of_task;
-			final int index = first_task_index;
-			first_task_index = (first_task_index+1) % tasks.length;
-			this.notify(); // wake up possible waiting threads in addTask
-			return tasks[index];
 		}
 
-		public synchronized int size() {
-			return nr_of_task;
+		public synchronized int size() throws InterruptedException {
+			monitor.lock.lockInterruptibly();
+			try {
+				return tasks.size();
+			} finally {
+				monitor.lock.unlock();
+			}
 		}
 	}
 
@@ -86,7 +78,7 @@ public class WorkerThreadDemo {
 						for (;;) {
 							int task = queue.removeTask();
 							System.out.println(getName() + ": Bearbeite Task " + task);
-							Thread.sleep(800 + 500 * rnd.nextInt(10));
+							Thread.sleep(900 + 500 * rnd.nextInt(10));
 						}
 					} catch (InterruptedException e) { 
 						System.out.println(getName() + ": Interrupted");
@@ -103,7 +95,7 @@ public class WorkerThreadDemo {
 		for (int task = 0; task < 20; ++task) {
 			System.out.println(Thread.currentThread().getName() + ": Erzeuge neue Task " + task);
 			queue.addTask(task);
-			Thread.sleep(100 + 100 * rnd.nextInt(10));
+			Thread.sleep(50 + 50 * rnd.nextInt(10));
 		}
 
 		while (queue.size() > 0) {

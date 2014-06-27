b7ab0e0ab271f0399dd75ed67e9fe30688f19bb3
Jörg Seebohn <joerg.seebohn@web.de>
2014-06-27 19:56:46 +0200

    Update syncfunc.c

--- syncfunc.c
+++ syncfunc.c
@@ -1,6 +1,6 @@
 /* title: SyncFunc impl
 
-   Implements <SyncFunc>.
+   Implementiere <SyncFunc>.
 
    about: Copyright
    This program is free software.
@@ -84,12 +84,33 @@ static int test_getset(void)
       }
    }
 
-   // TODO: TEST optstate_syncfunc
+   // TEST optstate_syncfunc
+   memset(&sfunc, 0, sizeof(sfunc));
+   for (syncfunc_opt_e opt1 = 0; opt1 <= syncfunc_opt_STATE; opt1 += syncfunc_opt_STATE) {
+      for (syncfunc_opt_e opt2 = 0; opt2 <= syncfunc_opt_CONTLABEL; opt2 += syncfunc_opt_CONTLABEL) {
+         for (uintptr_t i = 1024; i <= 4096; i *= 2) {
+            sfunc.state = (void*)i;
+            const void * expect = opt1 ? (void*)i : 0;
+            TEST(expect == optstate_syncfunc(&sfunc, opt1|opt2));
+         }
+      }
+   }
 
-   // TODO: TEST optcontlabel_syncfunc
-
+   // TEST optcontlabel_syncfunc
+   for (syncfunc_opt_e opt1 = 0; opt1 <= syncfunc_opt_STATE; opt1 += syncfunc_opt_STATE) {
+      for (syncfunc_opt_e opt2 = 0; opt2 <= syncfunc_opt_CONTLABEL; opt2 += syncfunc_opt_CONTLABEL) {
+         for (uintptr_t i = 1024; i <= 4096; i *= 2) {
+            memset(&sfunc, 0, sizeof(sfunc));
+            if (opt1) sfunc.contlabel = (void*)i;
+            else      sfunc.state     = (void*)i;
+            const void * expect = opt2 ? (void*)i : 0;
+            TEST(expect == optcontlabel_syncfunc(&sfunc, opt1|opt2));
+         }
+      }
+   }
 
    // TEST setstate_syncfunc
+   memset(&sfunc, 0, sizeof(sfunc));
    for (uintptr_t i = 1; i; i <<= 1) {
       setstate_syncfunc(&sfparam, (void*) i);
       TEST((void*)i == getstate_syncfunc(&sfparam));
@@ -98,6 +119,7 @@ static int test_getset(void)
    TEST(0 == getstate_syncfunc(&sfparam));
 
    // TEST setall_syncfunc: syncfunc_opt_NONE
+   memset(&sfunc, 0, sizeof(sfunc));
    setall_syncfunc(&sfunc, syncfunc_opt_NONE, &test_execmd_sf, (void*)2, (void*)3);
    TEST(sfunc.mainfct   == &test_execmd_sf);
    TEST(sfunc.state     == 0);
@@ -167,6 +189,93 @@ static int test_exit_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
    exit_syncfunc(sfparam, (int)sfcmd);
 }
 
+static int test_wait_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
+{
+   execmd_syncfunc(sfparam, sfcmd, ONRUN, ONERR);
+
+   goto ONERR;
+
+ONRUN: ;
+   int err = wait_syncfunc(sfparam, (void*)1);
+   if (err) goto ONERR;
+   err = wait_syncfunc(sfparam, (void*)2);
+   if (err) goto ONERR;
+   err = wait_syncfunc(sfparam, (void*)3);
+   if (err) goto ONERR;
+   return syncfunc_cmd_EXIT;
+
+ONERR:
+   return -1;
+}
+
+static int test_waiterr_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
+{
+   execmd_syncfunc(sfparam, sfcmd, ONRUN, ONERR);
+
+   goto ONERR;
+
+ONRUN: ;
+   intptr_t err = sfparam->waiterr;
+   err = wait_syncfunc(sfparam, (void*)err);
+   err = wait_syncfunc(sfparam, (void*)err);
+   err = wait_syncfunc(sfparam, (void*)err);
+   return syncfunc_cmd_EXIT;
+
+ONERR:
+   return -1;
+}
+
+static int test_waitexit_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
+{
+   execmd_syncfunc(sfparam, sfcmd, ONRUN, ONERR);
+
+   goto ONERR;
+
+ONRUN: ;
+   int retcode = sfparam->retcode;
+   int err;
+   sfparam->state = (void*)(intptr_t)retcode;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   if (err) goto ONERR;
+   sfparam->state = (void*)(intptr_t)retcode;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   if (err) goto ONERR;
+   sfparam->state = (void*)(intptr_t)retcode;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   if (err) goto ONERR;
+   sfparam->state = (void*)(intptr_t)retcode;
+   return syncfunc_cmd_EXIT;
+
+ONERR:
+   return -1;
+}
+
+static int test_waitexiterr_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
+{
+   execmd_syncfunc(sfparam, sfcmd, ONRUN, ONERR);
+
+   goto ONERR;
+
+ONRUN: ;
+   int retcode = sfparam->retcode;
+   intptr_t err = sfparam->waiterr;
+   sfparam->state = (void*)(intptr_t)retcode;
+   sfparam->syncrun = (void*)err;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   sfparam->state = (void*)(intptr_t)retcode;
+   sfparam->syncrun = (void*)err;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   sfparam->state = (void*)(intptr_t)retcode;
+   sfparam->syncrun = (void*)err;
+   err = waitexit_syncfunc(sfparam, &retcode);
+   sfparam->state = (void*)(intptr_t)retcode;
+   sfparam->syncrun = (void*)err;
+   return syncfunc_cmd_EXIT;
+
+ONERR:
+   return -1;
+}
+
 static int test_yield_sf(syncfunc_param_t * sfparam, uint32_t sfcmd)
 {
    execmd_syncfunc(sfparam, sfcmd, ONRUN, ONERR);
@@ -198,17 +307,21 @@ static int test_implsupport(void)
       TEST(sfparam.syncrun     == 0);
       TEST(sfparam.state       == 0);
       TEST(sfparam.contlabel   != 0);
+      TEST(sfparam.condition   == 0);
+      TEST(sfparam.waiterr     == 0);
       TEST(sfparam.retcode -10 == cmd);
    }
 
    // TEST execmd_syncfunc: invalid sfcmd value
    sfparam.contlabel = 0;
-   for (int cmd = syncfunc_cmd_EXIT+1; cmd <= syncfunc_cmd_EXIT+2; ++cmd) {
+   for (int cmd = syncfunc_cmd_WAIT; cmd <= syncfunc_cmd_WAIT+16; ++cmd) {
       sfparam.retcode = 0;
       TEST(-1 == test_execmd_sf(&sfparam, (syncfunc_cmd_e)cmd));
       TEST( 0 == sfparam.syncrun);
       TEST( 0 == sfparam.state);
       TEST( 0 == sfparam.contlabel);
+      TEST( 0 == sfparam.condition);
+      TEST( 0 == sfparam.waiterr);
       TEST(-1 == sfparam.retcode);
    }
 
@@ -216,24 +329,113 @@ static int test_implsupport(void)
    for (uint32_t cmd = 0; cmd <= 100000; cmd += 10000) {
       sfparam.retcode = -1;
       TEST(syncfunc_cmd_EXIT == test_exit_sf(&sfparam, cmd));
-      TEST(sfparam.retcode     == (int)cmd);
-      TEST(sfparam.syncrun     == 0);
-      TEST(sfparam.state       == 0);
-      TEST(sfparam.contlabel   == 0);
+      TEST(sfparam.syncrun   == 0);
+      TEST(sfparam.state     == 0);
+      TEST(sfparam.contlabel == 0);
+      TEST(sfparam.condition == 0);
+      TEST(sfparam.waiterr   == 0);
+      TEST(sfparam.retcode   == (int)cmd);
    }
 
-   // TODO: TEST  wait_syncfunc
+   // TEST wait_syncfunc: waiterr == 0
+   memset(&sfparam, 0, sizeof(sfparam));
+   for (intptr_t i = 1; i <= 4; ++i) {
+      void * oldlabel = sfparam.contlabel;
+      int    result   = i != 4 ? syncfunc_cmd_WAIT : syncfunc_cmd_EXIT;
+      sfparam.condition = 0;
+      TEST(result == test_wait_sf(&sfparam, i == 1 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST(0 == sfparam.syncrun);
+      TEST(0 == sfparam.state);
+      if (i != 4) {
+         TEST(oldlabel != sfparam.contlabel);
+         TEST((void*)i == sfparam.condition);
+      } else {
+         TEST(oldlabel == sfparam.contlabel);
+         TEST(0        == sfparam.condition);
+      }
+      TEST(0 == sfparam.waiterr);
+      TEST(0 == sfparam.retcode);
+   }
 
-   // TODO: TEST  waitexit_syncfunc
+   // TEST wait_syncfunc: waiterr != 0
+   memset(&sfparam, 0, sizeof(sfparam));
+   for (intptr_t i = 1; i <= 4; ++i) {
+      void * oldlabel = sfparam.contlabel;
+      int    result   = i != 4 ? syncfunc_cmd_WAIT : syncfunc_cmd_EXIT;
+      sfparam.condition = 0;
+      sfparam.waiterr   = i;
+      TEST(result == test_waiterr_sf(&sfparam, i == 1 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST(0 == sfparam.syncrun);
+      TEST(0 == sfparam.state);
+      if (i != 4) {
+         TEST(oldlabel != sfparam.contlabel);
+         TEST((void*)i == sfparam.condition);
+      } else {
+         TEST(oldlabel == sfparam.contlabel);
+         TEST(0        == sfparam.condition);
+      }
+      TEST(i == sfparam.waiterr);
+      TEST(0 == sfparam.retcode);
+   }
+
+   // TEST waitexit_syncfunc: waiterr == 0
+   memset(&sfparam, 0, sizeof(sfparam));
+   for (intptr_t i = 1; i <= 4; ++i) {
+      void * oldlabel = sfparam.contlabel;
+      int    result   = i != 4 ? syncfunc_cmd_WAIT : syncfunc_cmd_EXIT;
+      sfparam.state     = 0 ;
+      sfparam.condition = (void*)1;
+      sfparam.retcode   = (int) i;
+      TEST(result == test_waitexit_sf(&sfparam, i == 1 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST(0        == sfparam.syncrun);
+      TEST((void*)i == sfparam.state);
+      if (i != 4) {
+         TEST(oldlabel != sfparam.contlabel);
+         TEST(0        == sfparam.condition); // clears condition
+      } else {
+         TEST(oldlabel == sfparam.contlabel);
+         TEST((void*)1 == sfparam.condition);
+      }
+      TEST(0 == sfparam.waiterr);
+      TEST(i == sfparam.retcode);
+   }
+
+   // TEST waitexit_syncfunc: waiterr != 0
+   memset(&sfparam, 0, sizeof(sfparam));
+   for (intptr_t i = 1; i <= 4; ++i) {
+      void * oldlabel = sfparam.contlabel;
+      int    result   = i != 4 ? syncfunc_cmd_WAIT : syncfunc_cmd_EXIT;
+      sfparam.syncrun   = 0;
+      sfparam.state     = 0;
+      sfparam.condition = (void*)1;
+      sfparam.waiterr   = i;
+      sfparam.retcode   = (int) -i;
+      TEST(result == test_waitexiterr_sf(&sfparam, i == 1 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST((void*) i == sfparam.syncrun);
+      TEST((void*)-i == sfparam.state);
+      if (i != 4) {
+         TEST(oldlabel != sfparam.contlabel);
+         TEST(0        == sfparam.condition); // clears condition
+      } else {
+         TEST(oldlabel == sfparam.contlabel);
+         TEST((void*)1 == sfparam.condition);
+      }
+      TEST(i  == sfparam.waiterr);
+      TEST(-i == sfparam.retcode);
+   }
 
    // TEST yield_syncfunc
-   sfparam.contlabel = 0;
+   memset(&sfparam, 0, sizeof(sfparam));
    for (int i = 19; i <= 21; ++i) {
       void * oldlabel = sfparam.contlabel;
       int    result   = i != 21 ? syncfunc_cmd_CONTINUE : syncfunc_cmd_EXIT;
       sfparam.retcode = 0;
-      TEST(result   == test_yield_sf(&sfparam, i == 19 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST(result == test_yield_sf(&sfparam, i == 19 ? syncfunc_cmd_RUN : syncfunc_cmd_CONTINUE));
+      TEST(0 == sfparam.syncrun);
+      TEST(0 == sfparam.state);
       TEST(oldlabel != sfparam.contlabel);
+      TEST(0 == sfparam.condition);
+      TEST(0 == sfparam.waiterr);
       TEST(i == sfparam.retcode);
    }
 

2526704d3064958b31aab666cd50e2588b27d620
Jörg Seebohn <joerg.seebohn@web.de>
2014-07-11 08:58:54 +0200

    Update synclink.h

--- synclink.h
+++ synclink.h
@@ -94,14 +94,30 @@ void init_synclink(/*out*/synclink_t * __restrict__ slink, /*out*/synclink_t * _
  * werden auf 0 gesetzt. */
 void free_synclink(synclink_t * slink);
 
+// group: query
+
+/* function: isvalid_synclink
+ * Return (slink->link != 0). */
+int isvalid_synclink(const synclink_t * slink);
+
 // group: update
 
 /* function: relink_synclink
- * Verbindet slink->link mit slink.
+ * Verbindet slink->link->link mit slink.
+ * Stellt die Verbindung zu dem Nachbar wieder her, nachdem slink im Speicher verschoben wurde.
  *
  * Ungeprüfte Precondition:
- * - slink->link != 0 */
+ * o isvalid_synclink(slink) */
 void relink_synclink(synclink_t * __restrict__ slink);
+
+/* function: unlink_synclink
+ * Invalidiert slink->link->link, setzt ihn auf 0.
+ * slink selbst wird nicht verändert.
+ * Dies ist eine Optimierung gegenüber <free_synclink>.
+ *
+ * Unchecked Precondition:
+ * o isvalid_synclink(slink) */
+void unlink_synclink(synclink_t * __restrict__ slink);
 
 
 /* struct: synclinkd_t
@@ -125,19 +141,20 @@ void relink_synclink(synclink_t * __restrict__ slink);
  *
  * Nach dem Entfernen des letzten d-Link Nachbarn:
  *
- * >        ╭─────────────╮
- * >  ╭─────┤ synclinkd_t ├─────╮
- * >  | next╰─────────────╯prev |
- * >  ╰─────────────────────────╯
+ * >           ╭─────────────╮
+ * >           ┤ synclinkd_t ├
+ * >  0 == next╰─────────────╯prev == 0
  *
- * Diser Zustand kann mit <issingle_synclinkd> überprüft werden.
- *
- * Würde dieser Link im Speicher verschoben, dann müsste danach
- * <relinksingle_synclinkd> anstatt <relink_synclinkd> aufgerufen werden.
- *
+ * Unchecked Invariant:
+ * o (<prev> == 0 && <next> == 0) || (<prev> != 0 && <next> != 0)
  * */
 struct synclinkd_t {
+   // group: private fields
+   /* variable: prev
+    * Zeigt auf vorherigen Link Nachbarn. */
    synclinkd_t * prev;
+   /* variable: next
+    * Zeigt auf nächsten Link Nachbarn. */
    synclinkd_t * next;
 };
 
@@ -152,54 +169,83 @@ struct synclinkd_t {
  * Initialisiert zwei Link-Seiten zu einem verbundenen Link. */
 void init_synclinkd(/*out*/synclinkd_t * slink, /*out*/synclinkd_t * other);
 
+/* function: initprev_synclinkd
+ * Fügt prev vor Knoten slink ein. Der Link prev wird initialisiert und verbunden mit den Knoten
+ * oldprev=slink->prev und slink. Nach Rückkehr zeigt oldprev->next auf prev, prev->prev auf oldprev
+ * und prev->next auf slink und slink->prev auf prev. */
+void initprev_synclinkd(/*out*/synclinkd_t * prev, synclinkd_t * slink);
+
+/* function: initnext_synclinkd
+ * Fügt next nach Knoten slink ein. Der Link next wird initialisiert und verbunden mit den Knoten
+ * slink und oldnext=slink->next. Nach Rückkehr zeigt oldnext->prev auf next, next->next auf oldnext
+ * und next->prev auf slink und slink->next auf next. */
+void initnext_synclinkd(/*out*/synclinkd_t * next, synclinkd_t * slink);
+
+/* function: initself_synclinkd
+ * Initialisiert slink, so daß er auf sich selbst verlinkt.
+ * Ein, Link der auf sich selbst zeigt, kann nicht im Speicher verschoben werden,
+ * da <relink_synclinkd> nicht funktioniert. Erst der erneute Aufruf <initself_synclinkd>
+ * nach dem Verschieben im Speicher macht ihn wieder gültig.
+ * Nach der Initialisierung können mittels <initprev_synclinkd> bzw. <initnext_synclinkd> neue Knoten
+ * verlinkt werden. */
+void initself_synclinkd(/*out*/synclinkd_t * slink);
+
 /* function: free_synclinkd
- * Trennt slink aus einer Link-Kette heraus. */
+ * Trennt slink aus einer Link-Kette heraus.
+ * slink wird auf den Wert <synclinkd_FREE> gesetzt.
+ * Falls slink der vorletzte Eintrag in der Kette war (slink->next==slink->prev),
+ * werden slink->next->prev und slink->next->next auch auf 0 gestzt. */
 void free_synclinkd(synclinkd_t * slink);
 
 // group: query
 
-/* function: issingle_synclinkd
- * Gibt true zurück, wenn slink auf sich selbst zeigt.
- * return (slink->prev == slink); */
-void issingle_synclinkd(const synclinkd_t * slink);
+/* function: isvalid_synclinkd
+ * Return (slink->prev != 0). */
+int isvalid_synclinkd(const synclinkd_t * slink);
+
+/* function: isself_synclinkd
+ * Return (slink->prev == slink). */
+int isself_synclinkd(const synclinkd_t * slink);
 
 // group: update
-
-/* function: insertprev_synclinkd
- * Verbindet slink->prev mit prev->next und prev->prev mit next des Knotens, auf den slink->prev vorher zeigte.
- * Der Link prev wird initialisiert und verbunden mit den Knoten slink und slink->prev und den Links slink->prev
- * und slink->prev->next. */
-void insertprev_synclinkd(synclinkd_t * slink, /*out*/synclinkd_t * prev);
-
-/* function: insertnext_synclinkd
- * Verbindet slink->next mit next->prev und next->next mit prev des Knotens, auf den slink->next vorher zeigte.
- * Der Link next wird initialisiert und verbunden mit den Knoten slink und slink->next und den Links slink->next
- * und slink->next->prev. */
-void insertnext_synclinkd(synclinkd_t * slink, /*out*/synclinkd_t * next);
-
-/* function: removeprev_synclinkd
- * Entfernt Link slink->prev aus Kette. Dasselbe kann mit <free_synclinkd>(slink->prev) erreicht werden.
- * Der Ausgabeparameter prev wird auf den alten Wert von slink->prev gesetzt. */
-void removeprev_synclinkd(synclinkd_t * slink, /*out*/synclinkd_t ** prev);
-
-/* function: removenext_synclinkd
- * Entfernt Link slink->next aus Kette. Dasselbe kann mit <free_synclinkd>(slink->next) erreicht werden.
- * Der Ausgabeparameter next wird auf den alten Wert von slink->next gesetzt. */
-void removenext_synclinkd(synclinkd_t * slink, /*out*/synclinkd_t ** next);
 
 /* function: relink_synclinkd
  * Stellt die Verbindung zu den Nachbarn wieder her, nachdem slink im Speicher verschoben wurde.
  *
- * Precondition:
- * - Vor dem Verschieben im Speicher: issingle_synclinkd(slink)==false */
+ * Unchecked Precondition:
+ * o isvalid_synclinkd(slink) */
 void relink_synclinkd(synclinkd_t * slink);
 
-/* function: relinksingle_synclinkd
- * Stellt die Verbindung zu sich selbst wieder her, nachdem slink im Speicher verschoben wurde.
+/* function: unlink_synclinkd
+ * Trennt slink aus einer Link-Kette heraus.
+ * slink selbst wird nicht verändert.
+ * Dies ist eine Optimierung gegenüber <free_synclinkd>.
  *
- * Precondition:
- * - Vor dem Verschieben im Speicher: issingle_synclinkd(slink)==true */
-void relinksingle_synclinkd(synclinkd_t * slink);
+ * Unchecked Precondition:
+ * o isvalid_synclinkd(slink) */
+void unlink_synclinkd(synclink_t * __restrict__ slink);
+
+/* function: unlinkkeepself_synclinkd
+ * Trennt slink aus einer Link-Kette heraus.
+ * slink selbst wird nicht verändert.
+ * Dies ist eine Optimierung gegenüber <free_synclinkd>.
+ * Falls slink der vorletzte Knoten in der Kette ist,
+ * dann gilt <isself_synclinkd>(slink->next) nach Return.
+ *
+ * Unchecked Precondition:
+ * o isvalid_synclinkd(slink) */
+void unlinkkeepself_synclinkd(synclink_t * __restrict__ slink);
+
+/* function: spliceprev_synclinkd
+ * Fügt Liste der Knoten, auf die prev zeigt, vor Knoten slink ein.
+ * Nach Return zeigt slink->prev auf alten Wert von prev->prev und prev->prev zeigt
+ * alten Wert von slink->prev.
+ *
+ * Unchecked Precondition:
+ * o isvalid_synclinkd(slink)
+ * o isvalid_synclinkd(prev)
+ * */
+void spliceprev_synclinkd(synclinkd_t * prev, synclinkd_t * slink);
 
 
 // section: inline implementation
@@ -227,6 +273,11 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl->link = 0;                \
          } while (0)
 
+/* define: isvalid_synclink
+ * Implementiert <synclink_t.isvalid_synclink>. */
+#define isvalid_synclink(slink) \
+         ((slink)->link != 0)
+
 /* define: relink_synclink
  * Implementiert <synclink_t.relink_synclink>. */
 #define relink_synclink(slink) \
@@ -235,19 +286,26 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl->link->link = _sl;        \
          } while (0)
 
+/* define: unlink_synclink
+ * Implementiert <synclink_t.unlink_synclink>. */
+#define unlink_synclink(slink) \
+         do {                             \
+            synclink_t * _sl = (slink);   \
+            _sl->link->link = 0;          \
+         } while (0)
+
 // group: synclinkd_t
 
 /* define: free_synclinkd
  * Implementiert <synclinkd_t.free_synclinkd>. */
 #define free_synclinkd(slink) \
          do {                             \
-            synclinkd_t * _sl = (slink);  \
-            if (_sl->prev) {              \
-            _sl->next->prev = _sl->prev;  \
-            _sl->prev->next = _sl->next;  \
+            synclinkd_t * _sl2 = (slink); \
+            if (_sl2->prev) {             \
+               unlink_synclinkd(slink);   \
             }                             \
-            _sl->next = 0;                \
-            _sl->prev = 0;                \
+            _sl2->next = 0;               \
+            _sl2->prev = 0;               \
          } while (0)
 
 /* define: init_synclinkd
@@ -262,9 +320,9 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl2->prev = _sl1;            \
          } while (0)
 
-/* define: insertnext_synclinkd
- * Implementiert <synclinkd_t.insertnext_synclinkd>. */
-#define insertnext_synclinkd(slink, _next) \
+/* define: initnext_synclinkd
+ * Implementiert <synclinkd_t.initnext_synclinkd>. */
+#define initnext_synclinkd(_next, slink) \
          do {                             \
             synclinkd_t * _sl = (slink);  \
             synclinkd_t * _ne = (_next);  \
@@ -274,9 +332,9 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl->next = _ne;              \
          } while (0)
 
-/* define: insertprev_synclinkd
- * Implementiert <synclinkd_t.insertprev_synclinkd>. */
-#define insertprev_synclinkd(slink, _prev) \
+/* define: initprev_synclinkd
+ * Implementiert <synclinkd_t.initprev_synclinkd>. */
+#define initprev_synclinkd(_prev, slink) \
          do {                             \
             synclinkd_t * _sl = (slink);  \
             synclinkd_t * _pr = (_prev);  \
@@ -286,13 +344,26 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl->prev = _pr;              \
          } while (0)
 
-/* define: issingle_synclinkd
- * Implementiert <synclinkd_t.issingle_synclinkd>. */
-#define issingle_synclinkd(slink) \
-         ( __extension__ ({      \
-            const synclinkd_t *  \
-                  _sl = (slink); \
-            (_sl->prev == _sl);  \
+/* define: initself_synclinkd
+ * Implementiert <synclinkd_t.initself_synclinkd>. */
+#define initself_synclinkd(slink) \
+         do {                             \
+            synclinkd_t * _sl = (slink);  \
+            _sl->prev = _sl;              \
+            _sl->next = _sl;              \
+         } while (0)
+
+/* define: isvalid_synclinkd
+ * Implementiert <synclinkd_t.isvalid_synclinkd>. */
+#define isvalid_synclinkd(slink) \
+         ((slink)->prev != 0)
+
+/* define: isself_synclinkd
+ * Implementiert <synclinkd_t.isself_synclinkd>. */
+#define isself_synclinkd(slink) \
+         ( __extension__ ({               \
+            synclinkd_t * _sl = (slink);  \
+            _sl->prev == _sl;             \
          }))
 
 /* define: relink_synclinkd
@@ -304,41 +375,41 @@ void relinksingle_synclinkd(synclinkd_t * slink);
             _sl->next->prev = _sl;        \
          } while (0)
 
-/* define: relinksingle_synclinkd
- * Implementiert <synclinkd_t.relinksingle_synclinkd>. */
-#define relinksingle_synclinkd(slink) \
+/* define: spliceprev_synclinkd
+ * Implementiert <synclink_t.spliceprev_synclinkd>. */
+#define spliceprev_synclinkd(_prev, slink) \
          do {                             \
             synclinkd_t * _sl = (slink);  \
-            _sl->prev = _sl;              \
-            _sl->next = _sl;              \
+            synclinkd_t * _pr = (_prev);  \
+            synclinkd_t * _ppr;           \
+            _ppr = _pr->prev;             \
+            _pr->prev = _sl->prev;        \
+            _pr->prev->next = _pr;        \
+            _ppr->next = _sl;             \
+            _sl->prev = _ppr;             \
          } while (0)
 
-/* define: removeprev_synclinkd
- * Implementiert <synclinkd_t.removeprev_synclinkd>. */
-#define removeprev_synclinkd(slink, _prev) \
-         do {                             \
-            synclinkd_t * _sl = (slink);  \
-            synclinkd_t * _pr;            \
-            _pr = _sl->prev;              \
-            _sl->prev = _pr->prev;        \
-            _sl->prev->next = _sl;        \
-            _pr->prev = 0;                \
-            _pr->next = 0;                \
-            *(_prev) = _pr;               \
+/* define: unlink_synclinkd
+ * Implementiert <synclink_t.unlink_synclinkd>. */
+#define unlink_synclinkd(slink) \
+         do {                                \
+            synclinkd_t * _sl = (slink);     \
+            if (_sl->prev == _sl->next) {    \
+               _sl->next->prev = 0;          \
+               _sl->next->next = 0;          \
+            } else {                         \
+               _sl->next->prev = _sl->prev;  \
+               _sl->prev->next = _sl->next;  \
+            }                                \
          } while (0)
 
-/* define: removenext_synclinkd
- * Implementiert <synclinkd_t.removenext_synclinkd>. */
-#define removenext_synclinkd(slink, _next) \
+/* define: unlinkkeepself_synclinkd
+ * Implementiert <synclink_t.unlinkkeepself_synclinkd>. */
+#define unlinkkeepself_synclinkd(slink) \
          do {                             \
             synclinkd_t * _sl = (slink);  \
-            synclinkd_t * _ne;            \
-            _ne = _sl->next;              \
-            _sl->next = _ne->next;        \
-            _sl->next->prev = _sl;        \
-            _ne->prev = 0;                \
-            _ne->next = 0;                \
-            *(_next) = _ne;               \
+            _sl->next->prev = _sl->prev;  \
+            _sl->prev->next = _sl->next;  \
          } while (0)
 
 #endif

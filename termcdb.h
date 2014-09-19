0a546b80a7324e2a99e1489aa3d656bb590dc4db
Jörg Seebohn <joerg.seebohn@web.de>
2014-09-19 18:27:39 +0200

    Update termcdb.h

--- termcdb.h
+++ termcdb.h
@@ -1,33 +1,26 @@
-/* title: TerminalControlDatabase
+/* title: TerminalCapabilityDatabase
 
    Liefert korrekte Control-Codes (ASCII control character sequence)
    für die Ansteuerung eines Terminals abhängig von seinem Typ.
 
-   about: Copyright
-   This program is free software.
-   You can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   Copyright:
+   This program is free software. See accompanying LICENSE file.
 
    Author:
    (C) 2014 Jörg Seebohn
 
    file: C-kern/api/io/terminal/termcdb.h
-    Header file <TerminalControlDatabase>.
+    Header file <TerminalCapabilityDatabase>.
 
    file: C-kern/io/terminal/termcdb.c
-    Implementation file <TerminalControlDatabase impl>.
+    Implementation file <TerminalCapabilityDatabase impl>.
 */
 #ifndef CKERN_IO_TERMINAL_TERMCDB_HEADER
 #define CKERN_IO_TERMINAL_TERMCDB_HEADER
 
-#include "C-kern/api/memory/memstream.h"
+// forward
+struct memstream_t;
+struct memstream_ro_t;
 
 /* typedef: struct termcdb_t
  * Export <termcdb_t> into global namespace. */
@@ -38,8 +31,8 @@ typedef struct termcdb_t termcdb_t;
 typedef struct termcdb_key_t termcdb_key_t;
 
 /* enums: termcdb_col_e
- * Farbwerte, die als Argumente der Funktionen <termcdb_t.setfgcolor_termcdb>
- * und <termcdb_t.setbgcolor_termcdb> Verwendung finden.
+ * Farbwerte, die als Argumente der Funktionen <termcdb_t.fgcolor_termcdb>
+ * und <termcdb_t.bgcolor_termcdb> Verwendung finden.
  *
  * termcdb_col_BLACK      - Farbe Schwarz.
  * termcdb_col_RED        - Farbe Rot.
@@ -63,7 +56,7 @@ typedef enum termcdb_col_e {
    termcdb_col_NROFCOLOR
 } termcdb_col_e;
 
-/* enums: termcdb_keymr_e
+/* enums: termcdb_keynr_e
  * Special Key Number.
  *
  * termcdb_keynr_F1 - F1 Function Key
@@ -89,6 +82,7 @@ typedef enum termcdb_col_e {
  * termcdb_keynr_DOWN     - Down Arrow Key
  * termcdb_keynr_LEFT     - Left Arrow Key
  * termcdb_keynr_RIGHT    - Right Arrow Key
+ * termcdb_keynr_CENTER,  - Mittlere Keypad Taste. Im Number-Modus ist das die Taste '5'.
  * */
 typedef enum termcdb_keynr_e {
    termcdb_keynr_UNKNOWN,
@@ -104,8 +98,8 @@ typedef enum termcdb_keynr_e {
    termcdb_keynr_F10,
    termcdb_keynr_F11,
    termcdb_keynr_F12,
-   termcdb_keynr_BS,
-   termcdb_keynr_HOME, // POS1
+   termcdb_keynr_BS,       // Backspace
+   termcdb_keynr_HOME,     // Pos1 (German)
    termcdb_keynr_INS,
    termcdb_keynr_DEL,
    termcdb_keynr_END,
@@ -115,22 +109,29 @@ typedef enum termcdb_keynr_e {
    termcdb_keynr_DOWN,
    termcdb_keynr_RIGHT,
    termcdb_keynr_LEFT,
-   termcdb_keynr_CENTER, // center of keypad (5)
-
+   termcdb_keynr_CENTER,   // center of keypad ('5')
 } termcdb_keynr_e;
 
 /* enums: termcdb_keymod_e
- * Modifier Key Bit. */
-typedef enum termcdb_keymod__e {
+ * Bits, welche die gedrückten Zusatztasten (modifier keys) kodieren.
+ * Generell wird es nur von xterm unterstützt. Die Linuxconsole unterstützt
+ * nur die Tasten Shift-F1 bis Shift-F8.
+ * termcdb_keymod_NONE  - Keine Zusatztasten gedrückt.
+ * termcdb_keymod_SHIFT - Es wurde zusätzlich die Shift-Taste gedrückt.
+ * termcdb_keymod_ALT   - Es wurde zusätzlich die Alt-Taste gedrückt.
+ * termcdb_keymod_CTRL  - Es wurde zusätzlich die Ctrl-Taste (Strg - Steuerung auf Deutsch) gedrückt.
+ * termcdb_keymod_META  - Es wurde zusätzlich die Meta-Taste (sofern vorhanden) gedrückt.
+ * * */
+typedef enum termcdb_keymod_e {
    termcdb_keymod_NONE,
-   termcdb_keymod_SHIFT = 2,
+   termcdb_keymod_SHIFT = 1,
+   termcdb_keymod_ALT   = 2,
    termcdb_keymod_CTRL  = 4,
-   termcdb_keymod_ALT   = 8,
-   termcdb_keymod_META  = 16
+   termcdb_keymod_META  = 8
 } termcdb_keymod_e;
 
 /* enums: termcdb_id_e
- * TODO:
+ * Liste der unterstützten Terminaltypen.
  * termcdb_id_LINUXCONSOLE - "linux" Terminal.
  * termcdb_id_XTERM        - "xterm" Terminal. */
 typedef enum termcdb_id_e {
@@ -151,7 +152,7 @@ int unittest_io_terminal_termcdb(void);
 
 
 /* struct: termcdb_key_t
- * Definiert eine von <querykey_termcdb> erkannte Spezialtaste. */
+ * Definiert eine von <key_termcdb> erkannte Spezialtaste. */
 struct termcdb_key_t {
    // group: public fields
    /* variable: nr
@@ -171,21 +172,20 @@ struct termcdb_key_t {
 
 
 /* struct: termcdb_t
- * TODO: describe type
+ * Beschreibt Terminaltyp und implementiert typabhängige Ansteuerung der Textausgabe und Abfrage der Tastatur.
  *
  * Einschränkung:
- * Es werden nur Terminals des Typs "xterm" und "linux" unterstützt.
+ * Es werden nur Terminals aus der Liste <termcdb_id_e> unterstützt.
  *
  * Spalten(x) und Zeilen(y):
  * Die X Werte bezeichnen die Spalten,
- * die Y Werte die Zeilen, beide beginnend mit 1.
- * Die linke obere Ecke des Terminals wird mit den Werten (1,1)
- * adressiert. Der Parameter screensizey gibt die Anzahl Zeilen (nr of rows)
- * des Terminals an.
+ * die Y Werte die Zeilen, beide beginnend mit 0.
+ * Die linke obere Ecke des Terminals wird mit den Werten (0,0)
+ * adressiert.
  * */
 struct termcdb_t {
    /* variable: termid
-    * Interne nummer des Terminals beginnend von 0. */
+    * Interne Nummer des Terminals beginnend von 0. */
    const uint16_t  termid;
    /* variable: typelist
     * Liste von Typnamen, unter denen das Terminal bekannt ist, jeweils getrennt mit '|'.
@@ -196,87 +196,141 @@ struct termcdb_t {
 // group: lifetime
 
 /* function: new_termcdb
- * TODO: Describe Initializes object.
- *
- * Einschränkung:
- * Es werden nur Terminals des Typs "xterm" und "linux" unterstützt
- * Alternative Namen sind: "xterm-debian", "X11 terminal emulator", "linux console". */
+ * Allokiert statisch ein <termcdb_t> und gibt in termcdb einen Zeiger darauf zurück.
+ * Es werden nur Terminals aus <termcdb_id_e> unterstützt. */
 int new_termcdb(/*out*/termcdb_t ** termcdb, const termcdb_id_e termid);
 
 /* function: newfromtype_termcdb
- * TODO: Describe Initializes object.
+ * Wie <new_termcdb>, erwartet in type aber Rückgabewert von <type_terminal>.
  *
  * Einschränkung:
- * Es werden nur Terminals des Typs "xterm" und "linux" unterstützt
- * Alternative Namen sind: "xterm-debian", "X11 terminal emulator", "linux console". */
+ * Es werden nur die Typnamen "xterm" und "linux" unterstützt bzw.
+ * die Alternativen: "xterm-debian", "X11 terminal emulator" und "linux console". */
 int newfromtype_termcdb(/*out*/termcdb_t ** termcdb, const uint8_t * type);
 
 /* function: delete_termcdb
- * TODO: Describe Frees all associated resources. */
+ * Setzt termcdb auf 0. */
 int delete_termcdb(termcdb_t ** termcdb);
 
-// group: control-codes
+// group: query
 
-// TODO: add set replace mode
-// TODO: set keyboard application mode
+/* function: id_termcdb
+ * Gibt die interne ID des Terminals zurück - siehe <termcdb_id_e>. */
+termcdb_id_e id_termcdb(const termcdb_t * termcdb);
 
-int startedit_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+// group: write control-codes
 
-int endedit_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+// TODO: add replace/insert mode
 
-/* function: setkeypadnormal_termcdb
- * TODO: German
- * cursor and keypad keys could be configured for application and normal mode
- * in application mode the keypad keys '/','*','-','+','<cr>' generate also special kodes
- * which are not backed up with constants from termcdb_keynr_e.
- * Therefore during init call setkeypadnormal_termcdb to switch to normal mode. */
-int setkeypadnormal_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: startedit_termcdb
+ *
+ * Initialisiert 2 Dinge:
+ * - Speichert Zustand und schaltet - wenn möglich - auf einen Alternativschirm.
+ * - Schaltet Replace-Modus an.
+ * - Schaltet Linewrap aus (am Ende der Zeile werden die Zeichen abgeschnitten anstatt in die nächste Zeile zu gehen).
+ * - Cursor- and Keypadtasten werden in den Modus »normal« geschalten.
+ *   Der Modus »application« ist die Alternative, bei dem die Keypadtasten
+ *   sich durch alterniv gesendete Codes unterscheiden. Im Normalmodus
+ *   senden die Keypadtasten '/','*','-','+','<cr>' die aufgedruckten Symbole,
+ *   im Appmode Escapesequenzen zur Unterscheidung. */
+int startedit_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int movecursor_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, uint16_t cursorx, uint16_t cursory);
+/* function: endedit_termcdb
+ * TODO: */
+int endedit_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int clearline_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: movecursor_termcdb
+ * TODO: */
+int movecursor_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, unsigned cursorx, unsigned cursory);
 
-// cursor at 1, 1
-int clearscreen_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: clearline_termcdb
+ * TODO: */
+int clearline_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int cursoroff_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: clearendofline_termcdb
+ * Löscht von der aktuellen Cursorposition bis zum Zeilenende den Inhalt. */
+int clearendofline_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int cursoron_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: clearscreen_termcdb
+ * cursor at 0, 0
+ * TODO: */
+int clearscreen_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int setbold_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: cursoroff_termcdb
+ * TODO: */
+int cursoroff_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int setfgcolor_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, bool bright, uint8_t fgcolor);
+/* function: cursoron_termcdb
+ * TODO: */
+int cursoron_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int setbgcolor_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, bool bright, uint8_t bgcolor);
+/* function: bold_termcdb
+ * TODO: */
+int bold_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-int resetstyle_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: fgcolor_termcdb
+ * TODO: */
+int fgcolor_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, bool bright, unsigned fgcolor);
 
-// cursor undefined
-int setscrollregion_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, uint16_t starty, uint16_t endy);
+/* function: bgcolor_termcdb
+ * TODO: */
+int bgcolor_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, bool bright, unsigned bgcolor);
 
-// cursor undefined
-int resetscrollregion_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes);
+/* function: normtext_termcdb
+ * TODO: */
+int normtext_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-// cursor undefined
-int deletelines_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, uint16_t nroflines);
+/* function: scrollregion_termcdb
+ * cursor undefined
+ * TODO: */
+int scrollregion_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, unsigned starty, unsigned endy);
 
-// cursor undefined
-int insertlines_termcdb(const termcdb_t * termcdb, /*ret*/memstream_t * ctrlcodes, uint16_t nroflines);
+/* function: scrollregionoff_termcdb
+ * cursor undefined
+ * TODO: */
+int scrollregionoff_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-// group: keycodes
+/* function: scrollup_termcdb
+ * Scrollt eine Zeile nach oben, sofern der Cursor in der letzten Zeile steht.
+ * Mit der letzen Zeile ist die Höhe-1 des Terminals in Zeilen gemeint oder endy,
+ * falls <scrollregion_termcdb> angeschalten ist. */
+int scrollup_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
 
-/* function: querykey_termcdb
+/* function: scrolldown_termcdb
+ * Scrollt eine Zeile nach unten, sofern der Cursor in der ersten Zeile steht.
+ * Mit der ersten Zeile ist 0 gemeint oder starty, falls <scrollregion_termcdb> angeschalten ist. */
+int scrolldown_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
+
+/* function: delchar_termcdb
+ * Löscht das Zeichen, auf dem der Cursor steht. Zeichen rechts davon werden nach links geschoben.
+ * Am rechten Bildschirmrand erscheint eine Leerstelle. */
+int delchar_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes);
+
+/* function: dellines_termcdb
+ * cursor undefined
+ * TODO: */
+int dellines_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, unsigned  nroflines);
+
+/* function: inslines_termcdb
+ * cursor undefined
+ * TODO: */
+int inslines_termcdb(const termcdb_t * termcdb, /*ret*/struct memstream_t * ctrlcodes, unsigned  nroflines);
+
+// group: read keycodes
+
+/* function: key_termcdb
+ * Gibt in key die Beschreibung der gedrückten Spezialtaste zurück.
+ * Parameter keycodes referenziert den von der Tastatur gelesenen Bytestrom.
+ *
  * Returns:
  * ENODATA - Zu wenig Bytes in keycodes. Weder keycodes noch key wurde verändert.
  * EILSEQ  - Der Tastenkode beginnend von keycodes->next ist nicht bekannt.
- * 0       - Die Spezialtaste wurde erkannt. keycodes->next wurde iunkrementiert und key enthält die
- *           Nummer der erkannten Taste.
+ *           Die Funktion muss mit (keycodes->next+1) wieder aufgerufen werden.
+ * 0       - Die Spezialtaste wurde erkannt. keycodes->next wurde um die Anzahl an Bytes inkrementiert,
+ *           welche die Spezialtaste beschreibenm, und key enthält deren Beschreibung.
  *
  * Es wird *kein* Logeintrag im Falle eines Fehlers geschrieben. */
-int querykey_termcdb(const termcdb_t * termcdb, memstream_t * keycodes, /*out*/termcdb_key_t * key);
-
-// group: update
-// TODO: remove group
+int key_termcdb(const termcdb_t * termcdb, struct memstream_ro_t * keycodes, /*out*/termcdb_key_t * key);
 
 
 
@@ -286,6 +340,11 @@ int querykey_termcdb(const termcdb_t * termcdb, memstream_t * keycodes, /*out*/t
  * Implements <termcdb_t.delete_termcdb>. */
 #define delete_termcdb(termcdb) \
          (*(termcdb) = 0)
+
+/* define: id_termcdb
+ * Implements <termcdb_t.id_termcdb>. */
+#define id_termcdb(termcdb) \
+         ((termcdb)->termid)
 
 
 #endif

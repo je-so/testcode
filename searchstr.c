f556ffa0878cc1ed252b881ea0f9342b5ef89244
Jörg Seebohn <joerg.seebohn@web.de>
2014-10-18 14:51:22 +0200

    Update String Search (KMP + Boyer-Moore)

--- searchstr.c
+++ searchstr.c
@@ -14,7 +14,7 @@
  * If substr is not found 0 is returned. If substr is contained more than
  * once the occurrence with the highest address is reported (reverse order).
  *
- * Used algorithmus: 
+ * Used algorithmus:
  * Knuth-Morris-Pratt!
  *
  * Worst case:
@@ -48,12 +48,12 @@ static const uint8_t* rsearchstr(size_t size, const uint8_t data[size], uint8_t 
          spos = sidx[spos];
          while (spos < substrsize && substr[spos] != data[dpos]) {
             spos = sidx[spos];
-         } 
+         }
       }
       if (! spos) return data + dpos;
    }
 
-   return 0;   
+   return 0;
 }
 
 /* function: searchstr
@@ -62,7 +62,7 @@ static const uint8_t* rsearchstr(size_t size, const uint8_t data[size], uint8_t 
  * If substr is not found 0 is returned. If substr is contained more than
  * once the occurrence with the lowest address is reported.
  *
- * Used algorithmus: 
+ * Used algorithmus:
  * Simplified Boyer-Moore without Bad-Character-Heuristic.
  * Like KMP but comparing starts from end of substr (substr[substrsize-1]).
  *
@@ -75,17 +75,22 @@ static const uint8_t* rsearchstr(size_t size, const uint8_t data[size], uint8_t 
  */
 const uint8_t* searchstr(size_t size, const uint8_t data[size], uint8_t substrsize, const uint8_t substr[substrsize])
 {
-   // nr of positions substr is to shift right 
+   // nr of positions substr is to shift right
    // array is indexed by nr of matched positions starting from end
    uint8_t shift[substrsize];
+   const uint8_t* pos;
 
    if (substrsize < 2) {
       return substrsize ? memchr(data, substr[0], size) : 0;
    }
 
+   if (substrsize > size) {
+      return 0;
+   }
+
    {
-      uint8_t* pos = memrchr(substr, substr[substrsize-1], substrsize-1);
-      int lastoff = (int) (pos ? 1 + (pos - substr) : 0);
+      pos = memrchr(substr, substr[substrsize-1], substrsize-1);
+      int lastoff = (pos ? 1 + (int) (pos - substr) : 0);
 
       shift[0] = 1;
       shift[1] = (uint8_t) (substrsize - lastoff);
@@ -102,19 +107,16 @@ const uint8_t* searchstr(size_t size, const uint8_t data[size], uint8_t substrsi
 
       for (int nrmatched = 2; nrmatched < substrsize; ++nrmatched) {
          if (lastoff >= nrmatched) {
-            printf("lastoff = %d nrmatched = %d\n", lastoff, nrmatched);
             int dpos = lastoff-nrmatched+1;
             int spos = substrsize-nrmatched+1;
             while (dpos > 0) {
                --dpos;
                --spos;
-               printf("dpos = %d spos = %d\n", dpos, spos);
                if (substr[spos] != substr[dpos]) {
                   spos = sidx[spos];
-                  printf("next spos = %d\n", spos);
-   	          while (spos < substrsize && substr[spos] != substr[dpos]) {
-	             spos = sidx[spos];
-	          } 
+                  while (spos < substrsize && substr[spos] != substr[dpos]) {
+                     spos = sidx[spos];
+                  }
                }
                if (nrmatched == substrsize-spos) break;
             }
@@ -124,50 +126,59 @@ const uint8_t* searchstr(size_t size, const uint8_t data[size], uint8_t substrsi
       }
    }
 
-   for (int i = 0; i < substrsize; ++i)
-      printf("%d[%d]\n", shift[i], i);
+   const int substrsize1 = substrsize-1;
 
-   --substrsize;
-
-   const 
-   uint8_t* lsubstr = substr + substrsize;
-   size_t   pos = substrsize;
-   int slen = substrsize;
+   int eoff = 0;
    int skip = 0;
 
-   while (pos < size) {
-      int nrmatched = 0;
-      while (lsubstr[-nrmatched] == data[pos-(size_t)nrmatched]) {
-         if (nrmatched == slen) {
-            nrmatched += skip; // make worst case O(size)
-            slen = substrsize;
-            if (nrmatched >= substrsize) {
-               return data + pos - substrsize;
+   const uint8_t* const
+   endpos = data + size - substrsize1;
+   pos = data;
+
+   do {
+      int off = substrsize1;
+      while (substr[off] == pos[off]) {
+         if (off == eoff) {
+            if (off <= skip) {
+               return pos;
             }
+            off -= skip; // make worst case O(size) instead O(size*substrlen)
+            eoff = 0;
          }
-         ++nrmatched;
+         --off;
       }
-      slen = shift[nrmatched];
+      int nrmatched = substrsize1 - off;
+      int pincr = shift[nrmatched];
       skip = nrmatched;
-      pos += slen;
-      --slen;
-      printf("pos = %d [%d %d]\n", pos, nrmatched, shift[nrmatched]);
-   }
+      pos += pincr;
+      eoff = substrsize - pincr;
+   } while (pos < endpos);
 
    return 0;
 }
- 
+
 int main()
 {
    char * s = "ababbbcabaabbbabababbbababbba";
    char * p = "ababbba";
-   
+
    printf("rsrch %s\n", rsearchstr(strlen(s), s, strlen(p), p));
    printf("rsrch %s\n", rsearchstr(strlen(s)-1, s, strlen(p), p));
 
    const uint8_t* pos1 = searchstr(strlen(s), s, strlen(p), p);
    printf("srch %s\n", pos1);
-   printf("== %s\n", pos1+1);
    printf("srch %s\n", searchstr(strlen(pos1+1), pos1+1, strlen(p), p));
+
+   char data[10000];
+   memset(data, 'a', sizeof(data));
+   for (int i = 0; i < 10000; i += 10) {
+      data[i] = 'b';
+   }
+   data[10000-10] = 'a';
+   data[10000-11] = 'b';
+   p = "aaaaaaaaaa";
+   printf("srch %s\n", searchstr(sizeof(data), data, strlen(p), p));
+
    return 0;
 }
+

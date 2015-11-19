5325652c78a808c04df1da86792d5d6c8e5132b0
Jörg Seebohn <je-so@users.noreply.github.com>
2015-11-19 18:36:42 +0100

    Start prototype of context oriented parser.

---
+++ parser/context_oriented_parser.c
@@ -0,0 +1,146 @@
+/*
+ * Build Prototype of Context Oriented Parser.
+ *
+ * The context is considered a structured (or object-) data model.
+ *
+ * The text (linear representation of an idea) is read from left to right
+ * and transformed into a structured representation (usage of C structs).
+ *
+ * The structured data represents the already read text.
+ * Newly read text adds new data to the data model or transforms it.
+ *
+ * This simple prototype parses simple expressions with operator precedence.
+ *
+ * TODO: Describe expressions
+ *
+ * TODO: Describe context (structs used to represent already read text)
+ *
+ * TODO: implement parse_expression
+ *
+ *
+ * Compile with:
+ * > gcc -ocop context_oriented_parser.c
+ *
+ * Run with:
+ * > ./cop <filename>
+ *
+ * */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+   char * addr;
+   size_t len;
+   size_t line;
+   size_t col;
+} buffer_t;
+
+static /*err*/int read_buffer(/*out*/buffer_t* buffer, const char * filename)
+{
+   FILE * file = fopen(filename, "rb");
+   long size;
+
+   if (!file) {
+      fprintf(stderr, "Can not open file '%s'\n", filename);
+      return ENOENT;
+   }
+
+
+   if (0 != fseek(file, 0, SEEK_END) || 0 > (size = ftell(file)) || 0 != fseek(file, 0, SEEK_SET)) {
+      fprintf(stderr, "Can not determine length of file '%s'\n", filename);
+      fclose(file);
+      return ENOENT;
+   }
+
+   buffer->addr = malloc((size_t) size);
+   if (0 == buffer->addr) {
+      fprintf(stderr, "Out of memory\n");
+      fclose(file);
+      return ENOMEM;
+   }
+   buffer->len  = (size_t) size;
+   buffer->line = 0;
+   buffer->col  = 0;
+
+   size = (long) fread(buffer->addr, 1, buffer->len, file);
+
+   fclose(file);
+
+   if (buffer->len != (size_t) size) {
+      fprintf(stderr, "Can not read file '%s'\n", filename);
+      return EIO;
+   }
+
+   return 0;
+}
+
+static void free_buffer(buffer_t * buffer)
+{
+   free(buffer->addr);
+   memset(buffer, 0, sizeof(buffer));
+}
+
+static inline int nextchar(buffer_t * buffer)
+{
+   while (buffer->len) {
+      char c = buffer->addr[0];
+      ++ buffer->addr;
+      -- buffer->len;
+      ++ buffer->col; /* no multibyte support ! */
+      if (c == '\n') {
+         ++ buffer->line;
+         buffer->col = 0;
+         continue;
+      } else if (c == ' ' || c == '\t') {
+         continue;
+      }
+
+      return c;
+   }
+
+   return 0;
+}
+
+static inline int peekchar(buffer_t * buffer)
+{
+   int c = nextchar(buffer);
+
+   if (c) {
+      -- buffer->addr;
+      ++ buffer->len;
+      -- buffer->col;
+   }
+
+   return c;
+}
+
+static int parse_expression(buffer_t * buffer)
+{
+   return ENOSYS;
+}
+
+
+int main(int argc, const char * argv[])
+{
+   int err;
+   buffer_t buffer;
+
+   if (argc != 2) {
+      fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+      return EXIT_FAILURE;
+   }
+
+   if (read_buffer(&buffer, argv[1])) {
+      return EXIT_FAILURE;
+   }
+
+   err = parse_expression(&buffer);
+
+   free_buffer(&buffer);
+
+   return err ? EXIT_FAILURE : EXIT_SUCCESS;
+}

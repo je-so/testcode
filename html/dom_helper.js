30f8a46ba58d8175c3504ea915e86761da2144eb
Jörg Seebohn <je-so@users.noreply.github.com>
2018-11-24 20:42:49 +0100

    Update dom_helper.js

--- html/dom_helper.js
+++ html/dom_helper.js
@@ -1,57 +1,115 @@
 <script>
-   // dom library
-   function dom(selector) {
-      if (!(this instanceof dom)) {
-         return new dom(selector)
+// js-component-library
+// getter proto === Object.getPrototypeOf
+// function c === createElement
+// function q === querySelectorAll
+// function qs === querySelector
+// function qid === getElementById
+// function qtag === getElementsByTagName
+// function on === addEventListener
+// focument.jscomponentlib() === returns getElementsByTagName("script")[i] with value i so that its innerText contains "// js-component-library"
+(function () {
+   Object.defineProperty(
+      Object.getPrototypeOf({}), 'proto', { get: function() { return Object.getPrototypeOf(this) }, enumerable: true, configurable: false }
+   );
+   (function(a) {
+      for (_ of a) {
+         if (_.createElement) _.c = _.createElement
+         if (_.createTextNode) _.ct = document.createTextNode
+         if (_.querySelectorAll) _.q = _.querySelectorAll
+         if (_.querySelector) _.qs = _.querySelector
+         if (_.getElementById) _.qid = _.getElementById
+         if (_.getElementsByTagName) _.qtag = _.getElementsByTagName
+         if (_.addEventListener) _.on = _.addEventListener
       }
-      this.elems =  ( typeof selector === "string" ? document.querySelectorAll(selector)
-                   : selector.addEventListener || !selector.length ? [ selector ]
-                   : selector )
-   }
-   Object.assign(dom, {
-      // must be called every time an iframe is loaded
-      adaptIFrameHeight(iframe) {
-         const content = iframe.contentDocument.documentElement
-         iframe.style.height = content.offsetHeight + "px" // reduce size to content size
-         iframe.style.height = content.scrollHeight + "px" // add margins
+   })([document.proto,window.proto,document.createElement("iframe").proto.proto]);
+   document.proto.jscomponentlib=function() {
+      return this.qtag("script").find( s => {
+         return s.innerText.substring(0,30).search("// js-component-library")>=0;
+      })
+   };
+   // Extend iframe.proto with functions:
+   // - adaptHeight: changes height of iframe to size of contained document
+   // - observeHeight: adds timer to window of iframe to adapt height on size change
+   (function (_) {
+   // Must be called every time an iframe is loaded.
+   // Only supports iframes with same origin!
+      _.installlib=function() {
+         const s=this.contentDocument.createElement("script")
+         s.appendChild(this.contentDocument.createTextNode(document.jscomponentlib().innerText))
+         this.contentDocument.documentElement.appendChild(s)
+      };
+      _.adaptHeight=function() {
+         const content = this.contentDocument.documentElement
+         this.style.height = content.offsetHeight + "px" // reduce size to content size
+         this.style.height = content.scrollHeight + "px" // add margins
          const scroll_bar_height = content.scrollHeight - content.clientHeight
-         if (0 < scroll_bar_height && scroll_bar_height < 40) {
-            iframe.style.height = (content.scrollHeight + scroll_bar_height) + "px"
+         if (0 < scroll_bar_height) {
+            this.style.height = (content.scrollHeight + scroll_bar_height) + "px"
          }
-      },
-      // must be called every time an iframe is loaded
-      observeIFrameHeight(iframe) {
-         const content = iframe.contentDocument.documentElement
+      };
+      _.observeHeight=function(milliseconds=100) {
+         const content = this.contentDocument.documentElement
          const keys = [ "offsetHeight", "offsetHeight", "clientHeight" ]
-         let old = {}
+         const old = {}
          keys.forEach( key => old[key] = content[key])
-         let timer = setInterval( () => { 
-            if (timer && keys.some( key => content[key] != old[key])) {
-               this.adaptIFrameHeight(iframe); 
+         const timer = setInterval( () => {
+            if (keys.some( key => content[key] != old[key])) {
+               this.adaptHeight();
                keys.forEach( key => old[key] = content[key])
             }
-         }, 100)
-         this.on(iframe.contentWindow, "unload", () => { 
+         }, milliseconds)
+         this.contentWindow.addEventListener("unload", () => {
             clearInterval(timer)
-            timer = null
          })
-      },
-      on(el, event, eventHandler, optional) {
-         el.addEventListener(event, eventHandler, optional)
-      },
-   })
-   Object.assign(dom.prototype, {
-      foreach(callback) {  for (const el of this.elems) { callback(el) } return this },
-      get(i) { return this.elems[i] },
-      on(event, eventHandler, optional) { return this.foreach( (el) => { el.addEventListener(event, eventHandler, optional) } ) },
-      adaptIFrameHeight() { this.constructor.adaptIFrameHeight(this.get(0)) }
-   })
+      };
+   })(Object.getPrototypeOf(document.c("iframe")));
+   // Extends NodeListPrototype and HTMLCollectionPrototype with functions:
+   // - forEach(callback): Iterates every contained node and calls callback with node as parameter
+   (function (a) {
+      for (_ of a) {
+         _.forEach=function(fct) {
+            for (let i=0; i<this.length; ++i) {
+               fct(this[i])
+            }
+         };
+         _.find=function(fct) {
+            for (let i=0; i<this.length; ++i) {
+               if(fct(this[i])) return this[i];
+            }
+         };
+      }
+   })([document.c("iframe").q("iframe").proto,document.c("iframe").qtag("iframe").proto]);
+})();
 </script>
 
 <script>
    // example usage
-   var iframe = document.getElementsByTagName("iframe")[0]
-   dom.on(iframe, "load", function() {
+   var iframe = document.qtag("iframe")[0]
+   var div = document.qtag("div")[0]
+
+   window.on("message", receiveMessage);
+
+   function receiveMessage(event)
+   {
+      console.log("event.origin", event.origin, event)
+   }
+
+   div.on("scroll", function(e) {
+      console.log("div", "" + this, this.scrollLeft, e)
+      console.log("div offsetWidth", this.offsetWidth)
+      console.log("div scrollWidth", this.scrollWidth)
+      console.log("div clientWidth", this.clientWidth)
+      console.log("div scrollHeight", this.scrollHeight)
+      console.log("div clientHeight", this.clientHeight)
+      console.log("div offsetHeight", this.offsetHeight)
+   })
+
+   iframe.contentWindow.addEventListener("resize", function() {
+      console.log("=== on resize ===")
+   })
+
+   iframe.on("load", function() {
       console.log("=== onload ====")
       console.log("body offsetHeight", iframe.contentDocument.body.offsetHeight)
       console.log("body scrollHeight", iframe.contentDocument.body.scrollHeight)
@@ -59,8 +117,8 @@
       console.log("html offsetHeight", iframe.contentDocument.documentElement.offsetHeight)
       console.log("html scrollHeight", iframe.contentDocument.documentElement.scrollHeight)
       console.log("html clientHeight", iframe.contentDocument.documentElement.clientHeight)
-      dom.adaptIFrameHeight(iframe)
-      dom.observeIFrameHeight(iframe)
+      iframe.adaptHeight()
+      iframe.observeHeight()
       setTimeout( function() {
          console.log("html offsetHeight", iframe.contentDocument.documentElement.offsetHeight)
          console.log("html scrollHeight", iframe.contentDocument.documentElement.scrollHeight)
@@ -70,4 +128,6 @@
          console.log("html clientWidth", iframe.contentDocument.documentElement.clientWidth)
       }, 100)
    })
+
+   iframe.installlib()
 </script>

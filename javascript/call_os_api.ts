82251e96983152267c369c9c8b22e4951c868a6b
Jörg Seebohn <je-so@users.noreply.github.com>
2019-08-24 13:16:59 +0200

    Create call_os_api.ts

---
+++ javascript/call_os_api.ts
@@ -0,0 +1,9 @@
+import { OS_API } from "./os-api";
+
+const dirpath = '.'
+let matcher = new OS_API.FilePathMatcher("C-kern/api/math/**/*.h")
+let matcher2 = new OS_API.FilePathMatcher("C-kern/platform/**/*.c")
+
+OS_API.scanDir(dirpath, [matcher,matcher2]).then( files => {
+  console.log(files)
+}).catch( err => console.log(err))

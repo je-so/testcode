a0f6088be4363fb0ca3747e1703b253f1fc744c0
Jörg Seebohn <je-so@users.noreply.github.com>
2022-04-03 15:54:01 +0200

    Update flow.js

--- javascript/flow.js
+++ javascript/flow.js
@@ -22,31 +22,30 @@ class Graph {
       }
       return graphNode
    }
-   addDependency(toNode,...fromNodes) {
+   addDependency(toNode,fromNodes) {
       const toGraphNode=this.Node(toNode)
       toGraphNode.nrPrevNodes+=fromNodes.length
       fromNodes.forEach( fromNode => this.Node(fromNode).nextNodes.push(toGraphNode))
    }
    /** Tests for cycles and returns a random one if there is at least one else undefined. */
    get cycle() {
-      const possibleCycleNodes=this.nodes.filter( n => n.nrVisitedFromPrev!==n.nrPrevNodes)
-      const unvisited=new Set(possibleCycleNodes)
-      const visiting=new Set()
       let cycle=undefined
+      this.nodes.forEach( n => n.visit=(n.nrVisitedFromPrev!==n.nrPrevNodes ? 2 : 0)) // nodes which may be part of cycle are marked with visit=2
       const visit=(node) => { // depth first search
-         if (unvisited.delete(node)) {
-            visiting.add(node)
-            node.nextNodes.find(visit)
-            visiting.delete(node)
+         if (node.visit===2 /*unvisited*/) {
+            node.visit=1 // state: visiting
+            if (node.nextNodes.find(visit))
+               cycle.unshift(node.node)
+            node.visit=0 // state: done
          }
-         else if (visiting.has(node)) {
-            cycle=[...visiting,node].map(n => n.node)
-            while (cycle[0]!==node.node)
-               cycle.shift() // remove cause cycle starts with node and ends with node
-         }
+         else if (node.visit===1 /*visiting*/)
+            cycle=[node.node]
          return cycle
       }
-      return possibleCycleNodes.find(visit) ? cycle : undefined
+      if (this.nodes.find(visit))
+         while (cycle[0]!==cycle[cycle.length-1])
+            cycle.shift() // remove start node cause cycle starts and ends with same node (end node is part of cycle)
+      return cycle
    }
    determineOrder(processOrderedNode) {
       // breadth first traversal to determine order of nodes (from input nodes (source) to output nodes (sink))
@@ -65,8 +64,15 @@ class Graph {
 
 var nodes=[ {nr:0}, {nr:1}, {nr:2}, {nr:3} ]
 var graph=new Graph(nodeIndex=>nodeIndex)
-graph.addDependency(1,0,2,3,3)
-graph.determineOrder( (nodeIndex,order) => nodes[nodeIndex].order=order )
+// graph.addDependency(1,[0,2,3,3])
+graph.addDependency(1,[0])
+graph.addDependency(2,[1])
+graph.addDependency(3,[2])
+// graph.addDependency(3,[3])
+// graph.addDependency(1,[3])
+let cycle=graph.determineOrder( (nodeIndex,order) => nodes[nodeIndex].order=order )
+console.log(cycle)
+console.log(graph.cycle)
+
 nodes.sort( (n1,n2) => n1.order-n2.order)
 console.log("ordered nodes",nodes)
-

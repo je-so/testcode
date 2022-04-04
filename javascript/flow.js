6a62b5a762682afca8fdec12dd5ee047d8b9c364
Jörg Seebohn <je-so@users.noreply.github.com>
2022-04-04 08:39:50 +0200

    Update flow.js

--- javascript/flow.js
+++ javascript/flow.js
@@ -30,15 +30,15 @@ class Graph {
    /** Tests for cycles and returns a random one if there is at least one else undefined. */
    get cycle() {
       let cycle=undefined
-      this.nodes.forEach( n => n.visit=(n.nrVisitedFromPrev!==n.nrPrevNodes ? 2 : 0)) // nodes which may be part of cycle are marked with visit=2
+      this.nodes.forEach( n => n.order=(n.nrVisitedFromPrev!==n.nrPrevNodes ? 2 : 0)) // possible cycle nodes marked with order=2
       const visit=(node) => { // depth first search
-         if (node.visit===2 /*unvisited*/) {
-            node.visit=1 // state: visiting
+         if (node.order===2 /*unvisited*/) {
+            node.order=1 // state: visiting
             if (node.nextNodes.find(visit))
                cycle.unshift(node.node)
-            node.visit=0 // state: done
+            node.order=0 // state: done
          }
-         else if (node.visit===1 /*visiting*/)
+         else if (node.order===1 /*visiting*/)
             cycle=[node.node]
          return cycle
       }
@@ -47,9 +47,8 @@ class Graph {
             cycle.shift() // remove start node cause cycle starts and ends with same node (end node is part of cycle)
       return cycle
    }
-   determineOrder(processOrderedNode) {
-      // breadth first traversal to determine order of nodes (from input nodes (source) to output nodes (sink))
-      let visitNodes=this.nodes.filter( n => n.nrPrevNodes===0) // start with input nodes
+   determineOrder(processOrderedNode) { // breadth first traversal to determine order of nodes (from input nodes (source) to output nodes (sink))
+      let visitNodes=this.nodes.filter( n => (n.nrVisitedFromPrev=0,n.nrPrevNodes===0)) // start with input nodes (and reset nrVisitedFromPrev)
       let order=-1, nrVisited=0
       while ((++order, nrVisited+=visitNodes.length, visitNodes.length>0))
          visitNodes=visitNodes.flatMap( node => (node.order=order, node.nextNodes.filter( next => ++next.nrVisitedFromPrev === next.nrPrevNodes)))
@@ -66,13 +65,17 @@ var nodes=[ {nr:0}, {nr:1}, {nr:2}, {nr:3} ]
 var graph=new Graph(nodeIndex=>nodeIndex)
 // graph.addDependency(1,[0,2,3,3])
 graph.addDependency(1,[0])
-graph.addDependency(2,[1])
-graph.addDependency(3,[2])
+graph.addDependency(3,[1])
+graph.addDependency(2,[3])
 // graph.addDependency(3,[3])
 // graph.addDependency(1,[3])
 let cycle=graph.determineOrder( (nodeIndex,order) => nodes[nodeIndex].order=order )
-console.log(cycle)
-console.log(graph.cycle)
+console.log("determineOrder returns",cycle)
+console.log("cycle returns",graph.cycle)
+console.log("order of nodes",nodes.map(n=>n.order))
 
+// calling another time orders nodes a 2nd time
+nodes.forEach( n => n.order=-1 )
+graph.determineOrder( (nodeIndex,order) => nodes[nodeIndex].order=order )
 nodes.sort( (n1,n2) => n1.order-n2.order)
 console.log("ordered nodes",nodes)
